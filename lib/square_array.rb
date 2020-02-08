24bcb27b2eb91e125ba95db6f24f0c11dd4d860a
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-08 09:04:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   i = 0;
   while array[i] do 
     new_array = []
-    puts new_array << array[i] ** 2
+    print new_array << array[i] ** 2
     i += 1; 
   end 
   print new_array
